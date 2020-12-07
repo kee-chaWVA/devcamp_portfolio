8ef235097bcd0de226a231c659ec7242958e5a16
kee cha <kee@wisvis.com>
2020-12-07 17:22:34 -0600

    implemented authorization engine for portfolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,8 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -57,9 +59,10 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       technologies_attributes: [:name])
   end
+  
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
 
-def set_portfolio_item
-  @portfolio_item = Portfolio.find(params[:id])
-end
 
