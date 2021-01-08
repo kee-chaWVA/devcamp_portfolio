1ce4cc4c05b82b5dbae4cde4491016285dcb2ff8
kee cha <kee@wisvis.com>
2021-01-07 20:50:02 -0600

    implemented fixes for images and grid for portfolio items

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -42,6 +42,7 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
 
+
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Your post is now live.' }
